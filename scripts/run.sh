55b26d289181ae9f9564e181ef6c63ee6a03db28
Alexander Beckerman <beckermana@mail.tau.ac.il>
2024-08-24 15:56:57 +0300

    fixed run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,7 +10,11 @@ make
 # Step 2: Run the compiled C++ program to generate the moves file
 echo "Running the C++ program..."
 cd src/simulator
-./myrobot $1 $2 $3
+ARG1=${1-}
+ARG2=${2-}
+ARG3=${3-}
+ARG4=${4-}
+./myrobot  $ARG1 $ARG2 $ARG3 $ARG4
 
 # Return to starting folder
 cd ../../../
